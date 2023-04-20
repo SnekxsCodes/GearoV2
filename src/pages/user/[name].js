b13ee843522ff0615c8e8522ba2028a8742ecf94
Snekxs <creepergang321@gmail.com>
2023-04-20 14:45:29 -0600

    Updated User Page

--- src/pages/user/[name].js
+++ src/pages/user/[name].js
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../../components/helpers/supabase";
-
+import Header from "../../../components/Header";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Stack,
+  Heading,
+  Button,
+  Image,
+} from "@chakra-ui/react";
 export default function UserPage(name) {
   const [data, setData] = useState();
 
@@ -12,6 +23,7 @@ export default function UserPage(name) {
         .eq("name", name.name)
         .then((response) => {
           setData(response.data);
+          console.log(response.data);
         });
     }
     fetchData();
@@ -19,12 +31,116 @@ export default function UserPage(name) {
 
   return (
     <div>
+      <Header />
       {data?.map((user) => (
-        <div key={user.id}>
-          <h1>{user.name}</h1>
-          <h1>{user.mouse}</h1>
-          <h1>{user.mousepad}</h1>
-        </div>
+        <>
+          <div className="UserCard" key={user.id}>
+            <img src={user.image_url} />
+            <h1>{user.name}</h1>
+            <h1 className={"UserCreated"}>Created Yesterday</h1>
+            <div className="userStatus">
+              <h1>Online</h1>
+            </div>
+          </div>
+          <div className={"products"}>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src={user.mouse_image}
+                alt="mouse"
+              />
+
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{user.mouse}</Heading>
+                </CardBody>
+
+                <CardFooter>
+                  <Button variant="solid" colorScheme="blue">
+                    Link
+                  </Button>
+                </CardFooter>
+              </Stack>
+            </Card>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src={user.mousepad_image}
+                alt="mousepad"
+              />
+
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{user.mousepad}</Heading>
+                </CardBody>
+
+                <CardFooter>
+                  <Button variant="solid" colorScheme="blue">
+                    Link
+                  </Button>
+                </CardFooter>
+              </Stack>
+            </Card>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src={user.headphones_image}
+                alt="headphones"
+              />
+
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{user.headphones}</Heading>
+                </CardBody>
+
+                <CardFooter>
+                  <Button variant="solid" colorScheme="blue">
+                    Link
+                  </Button>
+                </CardFooter>
+              </Stack>
+            </Card>
+            <Card
+              direction={{ base: "column", sm: "row" }}
+              overflow="hidden"
+              variant="outline"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: "100%", sm: "200px" }}
+                src={user.monitor_image}
+                alt="monitor"
+              />
+
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{user.monitor}</Heading>
+                </CardBody>
+
+                <CardFooter>
+                  <Button variant="solid" colorScheme="blue">
+                    Link
+                  </Button>
+                </CardFooter>
+              </Stack>
+            </Card>
+          </div>
+        </>
       ))}
     </div>
   );
